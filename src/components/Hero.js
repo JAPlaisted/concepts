48724c9d2cd689ebaac17f4ef91db9ad127c3b84
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-30 12:34:50 -0700

    Hero Logo Edits

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ function Hero() {
     <div>
       <Nav />
       <Box className="hero">
-        <img className="logo" src={logo} alt="Synoptic logo"></img>
+        <img className="logo center" src={logo} alt="Synoptic logo"></img>
         <h4>Just what you're looking for</h4>
       </Box>
     </div>
