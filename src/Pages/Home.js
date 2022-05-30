290b80c9ca8e3493ace47edba05c1a6adb00762e
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-30 14:57:58 -0700

    useEffect scroll on Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,9 @@ import Articles from "../components/Articles";
 import Hero from "../components/Hero";
 
 function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <Hero />
