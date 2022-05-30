a204a92f68ed66ee6342934375e02fd4b470b63f
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-30 12:25:06 -0700

    Hero nav

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Box, Link } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import "./Hero.css";
 import logo from "../synoptic.png";
+import Nav from "../components/Nav";
 
 function Hero() {
   return (
     <div>
-      <AppBar className="appBar" color="transparent">
-        <Toolbar>
-          <Link to="/" rel="stylesheet" href="">
-            <Typography color="primary">TSB</Typography>
-          </Link>
-        </Toolbar>
-      </AppBar>
+      <Nav />
       <Box className="hero">
         <Box className="title--hero">
           <img className="logo" src={logo} alt="Synoptic logo"></img>
