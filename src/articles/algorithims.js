c0190603af6f7476055ff996f0ddce66a7aab539
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-29 20:49:05 -0700

    5 Best Programming Book Content

--- src/articles/algorithims.js
+++ src/articles/algorithims.js
@@ -43,74 +43,52 @@ function Algorithims() {
               5 Best Books to Learn Code
             </Typography>
             <Typography className="body__text" variant="h5" color="secondary">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-                consectetur, minima ipsam quam quis perspiciatis voluptates,
-                iste consequuntur, sed deleniti eum in dolore nostrum eaque
-                repellat hic culpa autem voluptatem ducimus eos aliquid
-                repudiandae ipsa eligendi? Veritatis, eaque quidem asperiores
-                explicabo suscipit iusto ab laboriosam, odio quam illo,
-                recusandae omnis et temporibus tempora totam odit dignissimos id
-                eius labore. Nulla ea quo numquam quas dolorum repellendus,
-                suscipit dolores. <br />
-                <br />
-                Quod libero praesentium fuga delectus maiores error aliquid
-                expedita cum dolore, consequuntur beatae doloremque consectetur
-                repellendus perferendis, excepturi optio aspernatur. Quae dolore
-                magnam aperiam exercitationem, accusamus libero repellat nostrum
-                cum officia ea dolorem a repudiandae deleniti praesentium,
-                aspernatur, iure modi id recusandae nemo! Ipsa natus nihil fuga
-                corrupti, officia impedit laborum voluptatem esse ut sed
-                eligendi id blanditiis adipisci quae eius necessitatibus magnam
-                vel excepturi. Officia nesciunt, minus velit reprehenderit
-                deserunt ipsum, itaque obcaecati, temporibus commodi vitae harum
-                porro! Reprehenderit corporis sapiente totam sunt est qui
-                quaerat unde quibusdam recusandae aperiam in quos aliquid natus
-                consequatur hic, itaque earum veniam reiciendis perspiciatis
-                dolorem beatae quas eos? <br />
-                <br />
-                Sed facere id ad deserunt in placeat dolor aliquid nulla at
-                laborum tempora voluptas repudiandae velit architecto, vero qui
-                corrupti, maiores ipsa possimus nam neque accusantium! Suscipit
-                ex, delectus necessitatibus nihil quisquam hic aut fugiat
-                corporis quibusdam soluta debitis molestiae architecto qui optio
-                expedita magnam labore officia provident iste non alias quasi
-                incidunt commodi nam. Vero animi doloremque quam repudiandae
-                aliquid vitae eaque modi aliquam molestiae. Expedita illum a
-                molestias praesentium laboriosam sint omnis commodi ut. <br />
-                <br /> Ex magni quidem saepe amet voluptatem ea accusamus odio
-                molestias sint ab tenetur necessitatibus consectetur nostrum
-                eius eos sequi voluptas, consequuntur exercitationem explicabo
-                a! Deleniti fugiat fugit est magnam eveniet beatae, quaerat nam
-                laborum ducimus ipsa alias, aperiam enim nobis ut consequatur
-                eum inventore quasi, ab voluptatum neque repudiandae provident
-                facilis aut quidem. Tempore rerum esse cum non doloremque sequi
-                minima ipsam dolorum aliquid! Ab vel incidunt quibusdam quod
-                saepe facere natus perspiciatis unde odio veritatis, suscipit,
-                harum asperiores id eum obcaecati quo commodi quos cum ad
-                repellat nulla beatae minus iusto. Voluptatibus sapiente
-                corrupti perspiciatis asperiores excepturi labore unde? Eum,
-                nihil aspernatur optio in consequatur alias necessitatibus
-                inventore tempore mollitia. A nobis vel veritatis possimus alias
-                odit nesciunt, accusantium pariatur! Molestiae quos cupiditate
-                animi nulla vel aspernatur enim nostrum optio minus quis,
-                quaerat hic, deserunt nemo adipisci. Explicabo perspiciatis eius
-                numquam dolores vitae doloribus, amet nulla recusandae corrupti
-                velit vel totam nam debitis quod tempore architecto! Vel
-                laboriosam dolorem excepturi, explicabo totam impedit minima
-                quis sint id, corrupti animi reprehenderit rem? Iusto, modi
-                reiciendis enim consequatur magnam maiores fugit magni esse
-                illum ipsa quas tenetur beatae inventore? <br />
-                <br />
-                Asperiores, voluptatibus tempora illum nostrum unde animi hic,
-                rerum atque necessitatibus, incidunt suscipit amet minima ut ad
-                ipsum libero sit! Culpa debitis repellat quaerat, sequi
-                provident, eos alias in labore totam molestiae sed voluptates
-                pariatur minus laborum dolorem asperiores atque illo autem quod
-                modi natus, praesentium doloribus quibusdam nesciunt. Qui
-                eligendi, assumenda iure consequuntur earum inventore numquam
-                aliquam suscipit sapiente ad nobis.
-              </p>
+              <ol type="1">
+                <li>
+                  <a
+                    href="https://www.amazon.com/Eloquent-JavaScript-3rd-Introduction-Programming/dp/1593279507/ref=sr_1_1?crid=2OEPALJF1VL57&keywords=eloquent+javascript&qid=1653882131&sprefix=eloquent%2Caps%2C168&sr=8-1"
+                    target="_blank"
+                  >
+                    Eloquent JavaScript
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a
+                    href="https://www.amazon.com/Pragmatic-Programmer-Anniversary-Journey-Mastery/dp/B0833FBNHV/ref=zg_bs_4133_1/145-3515772-1087942?pd_rd_i=B0833FBNHV&psc=1"
+                    target="_blank"
+                  >
+                    The Pragmatic Programmer
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a
+                    href="https://www.amazon.com/Code-Language-Computer-Hardware-Software/dp/0735611319/ref=sr_1_1?crid=311KV0GGJ7D3Y&keywords=code+the+hidden+language+of+computer+hardware+and+software&qid=1653882301&s=audible&sprefix=code+the+hidden%2Caudible%2C126&sr=1-1"
+                    target="_blank"
+                  >
+                    Code
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a
+                    href="https://www.amazon.com/Algorithms-MIT-Press-Essential-Knowledge/dp/0262539020/ref=sr_1_1?crid=3BOX52UYMEBJC&keywords=algorithms+mit&qid=1653882410&s=audible&sprefix=algorithims+mi%2Caudible%2C151&sr=1-1-catcorr"
+                    target="_blank"
+                  >
+                    Algorithms
+                  </a>
+                </li>
+                <li>
+                  {" "}
+                  <a
+                    href="https://www.amazon.in/Clean-Code-Robert-C-Martin/dp/8131773388/"
+                    target="_blank"
+                  >
+                    Clean Code
+                  </a>
+                </li>
+              </ol>
             </Typography>
           </CardContent>
         </CardActionArea>
