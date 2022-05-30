6f948a13b33fd0c94d5f5e93179bb3e4ce8ce9f8
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-30 10:06:25 -0700

    10 Rand Business Edits

--- src/articles/loops.js
+++ src/articles/loops.js
@@ -44,16 +44,77 @@ function Loops() {
             </Typography>
             <Typography className="body__text" variant="h5" color="secondary">
               <ol type="1">
-                <li>Airbnb For Parking Spaces.</li>
-                <li>Influencer Brand Negotiation Agency.</li>
-                <li>Freelance/Ghost-Written Book Publishing.</li>
-                <li>3D Printing Home Construction.</li>
-                <li>Custom Parts Shop.</li>
-                <li>Business Partner "Matching" App.</li>
-                <li>Vacant Real Estate Holding Company.</li>
-                <li>Gourmet Soap.</li>
-                <li>Real Estate Photography.</li>
-                <li>Company Automation Software</li>
+                <li>
+                  Airbnb For Parking Spaces.
+                  <br />
+                  <sub>
+                    Connect people who have extra lot space with those who need
+                    parking.
+                  </sub>
+                </li>
+                <li>
+                  Influencer Brand Negotiation Agency.
+                  <br />
+                  <sub>
+                    Find influencers and bring them sponsors and brand deals.
+                  </sub>
+                </li>
+                <li>
+                  Freelance/Ghost-Written Book Publishing.
+                  <br />
+                  <sub>Hire writers on Upwork and focus on publishing.</sub>
+                </li>
+                <li>
+                  3D Printing Home Construction.
+                  <br />
+                  <sub>
+                    Build and sell 3D printed homes with a concrete 3D printer.
+                  </sub>
+                </li>
+                <li>
+                  Custom Parts Shop.
+                  <br />
+                  <sub>
+                    With the technical know-how and some CNC machines, make
+                    custom parts.
+                  </sub>
+                </li>
+                <li>
+                  Business Partner "Matching" App.
+                  <br />
+                  <sub>
+                    Like Tinder, but for finding your dream startup cofounders.
+                  </sub>
+                </li>
+                <li>
+                  Vacant Real Estate Holding Company.
+                  <br />
+                  <sub>
+                    Buy and hold vacant land, let it raise in value, enjoy no
+                    liability.
+                  </sub>
+                </li>
+                <li>
+                  Gourmet Soap.
+                  <br />
+                  <sub>
+                    Make gourmet soap to sell online or at farmers markets.
+                  </sub>
+                </li>
+                <li>
+                  Real Estate Photography.
+                  <br />
+                  <sub>
+                    Armed with camera and drone, reach out to local realtors for
+                    opportunities.
+                  </sub>
+                </li>
+                <li>
+                  Company Automation Software. <br />
+                  <sub>
+                    Build company automation tools to save companies money.
+                  </sub>
+                </li>
               </ol>
             </Typography>
           </CardContent>
