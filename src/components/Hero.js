0f552db81b3a762ccba114a9f6845ede1a57b351
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-31 12:37:56 -0700

    Hero h4

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ function Hero() {
       <Nav />
       <Box className="hero">
         <img className="logo center" src={logo} alt="Synoptic logo"></img>
-        <h4>Just what you're looking for</h4>
+        <h4 className="center">Just the info you're looking for</h4>
       </Box>
     </div>
   );
