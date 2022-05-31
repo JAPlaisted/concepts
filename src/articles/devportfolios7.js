d57f07640065caa555a53deeb0150a83ae2f3210
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-31 13:43:28 -0700

    7 Devs article image

--- src/articles/devportfolios7.js
+++ src/articles/devportfolios7.js
@@ -23,8 +23,8 @@ function DevPortfolios7() {
           <CardMedia
             className="media__single"
             component="img"
-            image="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            title=""
+            image="https://miro.medium.com/max/1123/1*6C702o6cpNyLm27WLdZyjg.png"
+            title="Dev Portfolio"
           />
           <CardContent className="content__single">
             <Typography
