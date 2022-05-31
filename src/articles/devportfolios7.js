ad574c55e37aef582ae6f9391c94d127aee8deb2
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-31 14:54:56 -0700

    7 devs content

--- src/articles/devportfolios7.js
+++ src/articles/devportfolios7.js
@@ -35,74 +35,71 @@ function DevPortfolios7() {
               7 Developer Portfolios for Inspiration
             </Typography>
             <Typography className="body__text" variant="h5" color="secondary">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-                consectetur, minima ipsam quam quis perspiciatis voluptates,
-                iste consequuntur, sed deleniti eum in dolore nostrum eaque
-                repellat hic culpa autem voluptatem ducimus eos aliquid
-                repudiandae ipsa eligendi? Veritatis, eaque quidem asperiores
-                explicabo suscipit iusto ab laboriosam, odio quam illo,
-                recusandae omnis et temporibus tempora totam odit dignissimos id
-                eius labore. Nulla ea quo numquam quas dolorum repellendus,
-                suscipit dolores. <br />
-                <br />
-                Quod libero praesentium fuga delectus maiores error aliquid
-                expedita cum dolore, consequuntur beatae doloremque consectetur
-                repellendus perferendis, excepturi optio aspernatur. Quae dolore
-                magnam aperiam exercitationem, accusamus libero repellat nostrum
-                cum officia ea dolorem a repudiandae deleniti praesentium,
-                aspernatur, iure modi id recusandae nemo! Ipsa natus nihil fuga
-                corrupti, officia impedit laborum voluptatem esse ut sed
-                eligendi id blanditiis adipisci quae eius necessitatibus magnam
-                vel excepturi. Officia nesciunt, minus velit reprehenderit
-                deserunt ipsum, itaque obcaecati, temporibus commodi vitae harum
-                porro! Reprehenderit corporis sapiente totam sunt est qui
-                quaerat unde quibusdam recusandae aperiam in quos aliquid natus
-                consequatur hic, itaque earum veniam reiciendis perspiciatis
-                dolorem beatae quas eos? <br />
-                <br />
-                Sed facere id ad deserunt in placeat dolor aliquid nulla at
-                laborum tempora voluptas repudiandae velit architecto, vero qui
-                corrupti, maiores ipsa possimus nam neque accusantium! Suscipit
-                ex, delectus necessitatibus nihil quisquam hic aut fugiat
-                corporis quibusdam soluta debitis molestiae architecto qui optio
-                expedita magnam labore officia provident iste non alias quasi
-                incidunt commodi nam. Vero animi doloremque quam repudiandae
-                aliquid vitae eaque modi aliquam molestiae. Expedita illum a
-                molestias praesentium laboriosam sint omnis commodi ut. <br />
-                <br /> Ex magni quidem saepe amet voluptatem ea accusamus odio
-                molestias sint ab tenetur necessitatibus consectetur nostrum
-                eius eos sequi voluptas, consequuntur exercitationem explicabo
-                a! Deleniti fugiat fugit est magnam eveniet beatae, quaerat nam
-                laborum ducimus ipsa alias, aperiam enim nobis ut consequatur
-                eum inventore quasi, ab voluptatum neque repudiandae provident
-                facilis aut quidem. Tempore rerum esse cum non doloremque sequi
-                minima ipsam dolorum aliquid! Ab vel incidunt quibusdam quod
-                saepe facere natus perspiciatis unde odio veritatis, suscipit,
-                harum asperiores id eum obcaecati quo commodi quos cum ad
-                repellat nulla beatae minus iusto. Voluptatibus sapiente
-                corrupti perspiciatis asperiores excepturi labore unde? Eum,
-                nihil aspernatur optio in consequatur alias necessitatibus
-                inventore tempore mollitia. A nobis vel veritatis possimus alias
-                odit nesciunt, accusantium pariatur! Molestiae quos cupiditate
-                animi nulla vel aspernatur enim nostrum optio minus quis,
-                quaerat hic, deserunt nemo adipisci. Explicabo perspiciatis eius
-                numquam dolores vitae doloribus, amet nulla recusandae corrupti
-                velit vel totam nam debitis quod tempore architecto! Vel
-                laboriosam dolorem excepturi, explicabo totam impedit minima
-                quis sint id, corrupti animi reprehenderit rem? Iusto, modi
-                reiciendis enim consequatur magnam maiores fugit magni esse
-                illum ipsa quas tenetur beatae inventore? <br />
-                <br />
-                Asperiores, voluptatibus tempora illum nostrum unde animi hic,
-                rerum atque necessitatibus, incidunt suscipit amet minima ut ad
-                ipsum libero sit! Culpa debitis repellat quaerat, sequi
-                provident, eos alias in labore totam molestiae sed voluptates
-                pariatur minus laborum dolorem asperiores atque illo autem quod
-                modi natus, praesentium doloribus quibusdam nesciunt. Qui
-                eligendi, assumenda iure consequuntur earum inventore numquam
-                aliquam suscipit sapiente ad nobis.
-              </p>
+              <ul>
+                <li>
+                  <a href="https://lynnandtonic.com/">
+                    <h4>Lynn Fisher</h4>
+                    <img
+                      href="https://alvarotrigo.com/blog/assets/imgs/2021-10-16/lynn-fisher-web-developer-portfolio-example.webp"
+                      alt="portfolio"
+                    ></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://findmatthew.com/">
+                    <h4>Matthew Williams</h4>
+                    <img
+                      href="https://alvarotrigo.com/blog/assets/imgs/2021-10-16/matthew-williams-web-developer-portfolio.webp"
+                      alt="portfolio"
+                    ></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.jonathanplaisted.com/">
+                    <h4>Jonathan Plaisted</h4>
+                    <img
+                      href="https://user-images.githubusercontent.com/55415399/163905022-44cf83c9-942c-4753-ae7e-79742417c921.PNG"
+                      alt="portfolio"
+                    ></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://mattfarley.ca/">
+                    <h4>Matt Farley</h4>
+                    <img
+                      href="https://cdn-media-1.freecodecamp.org/images/1*FNvXAOU_leQ7E2Bjuc8gqw.png"
+                      alt="portfolio"
+                    ></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.adhamdannaway.com/">
+                    <h4>Adham Dannaway</h4>
+                    <img
+                      href="https://cdn.hashnode.com/res/hashnode/image/upload/v1618299482987/s7c24s_NN.png?auto=compress,format&format=webp"
+                      alt="portfolio"
+                    ></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.seanhalpin.design/">
+                    <h4>Sean Halpin</h4>
+                    <img
+                      href="https://miro.medium.com/max/1400/0*HmDsRf97mn1Sasi_.png"
+                      alt="portfolio"
+                    ></img>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://taylorkmho.com/">
+                    <h4>Taylor Ho</h4>
+                    <img
+                      href="https://lh4.googleusercontent.com/cbXcGHgNAq5bzA4-CKi7GUdX3nw6XB7M-NNrPCqHyMJzVos1rguu2Gg2TZ4ZNL37Y0Yj6TlDga9C8vxxtmUGSUPuiySK4pI0vpWsbIKzfnUMfoWw3P4BcFssLgHzUMWLia-AMF_f"
+                      alt="portfolio"
+                    ></img>
+                  </a>
+                </li>
+              </ul>
             </Typography>
           </CardContent>
         </CardActionArea>
