ba3edd523889c3670d4d5f638e7f0ece2459f873
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-31 14:00:11 -0700

    dividing by zero article content

--- src/articles/dividingby0.js
+++ src/articles/dividingby0.js
@@ -36,72 +36,28 @@ function DividingBy0() {
             </Typography>
             <Typography className="body__text" variant="h5" color="secondary">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-                consectetur, minima ipsam quam quis perspiciatis voluptates,
-                iste consequuntur, sed deleniti eum in dolore nostrum eaque
-                repellat hic culpa autem voluptatem ducimus eos aliquid
-                repudiandae ipsa eligendi? Veritatis, eaque quidem asperiores
-                explicabo suscipit iusto ab laboriosam, odio quam illo,
-                recusandae omnis et temporibus tempora totam odit dignissimos id
-                eius labore. Nulla ea quo numquam quas dolorum repellendus,
-                suscipit dolores. <br />
-                <br />
-                Quod libero praesentium fuga delectus maiores error aliquid
-                expedita cum dolore, consequuntur beatae doloremque consectetur
-                repellendus perferendis, excepturi optio aspernatur. Quae dolore
-                magnam aperiam exercitationem, accusamus libero repellat nostrum
-                cum officia ea dolorem a repudiandae deleniti praesentium,
-                aspernatur, iure modi id recusandae nemo! Ipsa natus nihil fuga
-                corrupti, officia impedit laborum voluptatem esse ut sed
-                eligendi id blanditiis adipisci quae eius necessitatibus magnam
-                vel excepturi. Officia nesciunt, minus velit reprehenderit
-                deserunt ipsum, itaque obcaecati, temporibus commodi vitae harum
-                porro! Reprehenderit corporis sapiente totam sunt est qui
-                quaerat unde quibusdam recusandae aperiam in quos aliquid natus
-                consequatur hic, itaque earum veniam reiciendis perspiciatis
-                dolorem beatae quas eos? <br />
-                <br />
-                Sed facere id ad deserunt in placeat dolor aliquid nulla at
-                laborum tempora voluptas repudiandae velit architecto, vero qui
-                corrupti, maiores ipsa possimus nam neque accusantium! Suscipit
-                ex, delectus necessitatibus nihil quisquam hic aut fugiat
-                corporis quibusdam soluta debitis molestiae architecto qui optio
-                expedita magnam labore officia provident iste non alias quasi
-                incidunt commodi nam. Vero animi doloremque quam repudiandae
-                aliquid vitae eaque modi aliquam molestiae. Expedita illum a
-                molestias praesentium laboriosam sint omnis commodi ut. <br />
-                <br /> Ex magni quidem saepe amet voluptatem ea accusamus odio
-                molestias sint ab tenetur necessitatibus consectetur nostrum
-                eius eos sequi voluptas, consequuntur exercitationem explicabo
-                a! Deleniti fugiat fugit est magnam eveniet beatae, quaerat nam
-                laborum ducimus ipsa alias, aperiam enim nobis ut consequatur
-                eum inventore quasi, ab voluptatum neque repudiandae provident
-                facilis aut quidem. Tempore rerum esse cum non doloremque sequi
-                minima ipsam dolorum aliquid! Ab vel incidunt quibusdam quod
-                saepe facere natus perspiciatis unde odio veritatis, suscipit,
-                harum asperiores id eum obcaecati quo commodi quos cum ad
-                repellat nulla beatae minus iusto. Voluptatibus sapiente
-                corrupti perspiciatis asperiores excepturi labore unde? Eum,
-                nihil aspernatur optio in consequatur alias necessitatibus
-                inventore tempore mollitia. A nobis vel veritatis possimus alias
-                odit nesciunt, accusantium pariatur! Molestiae quos cupiditate
-                animi nulla vel aspernatur enim nostrum optio minus quis,
-                quaerat hic, deserunt nemo adipisci. Explicabo perspiciatis eius
-                numquam dolores vitae doloribus, amet nulla recusandae corrupti
-                velit vel totam nam debitis quod tempore architecto! Vel
-                laboriosam dolorem excepturi, explicabo totam impedit minima
-                quis sint id, corrupti animi reprehenderit rem? Iusto, modi
-                reiciendis enim consequatur magnam maiores fugit magni esse
-                illum ipsa quas tenetur beatae inventore? <br />
-                <br />
-                Asperiores, voluptatibus tempora illum nostrum unde animi hic,
-                rerum atque necessitatibus, incidunt suscipit amet minima ut ad
-                ipsum libero sit! Culpa debitis repellat quaerat, sequi
-                provident, eos alias in labore totam molestiae sed voluptates
-                pariatur minus laborum dolorem asperiores atque illo autem quod
-                modi natus, praesentium doloribus quibusdam nesciunt. Qui
-                eligendi, assumenda iure consequuntur earum inventore numquam
-                aliquam suscipit sapiente ad nobis.
+                Currently, if you try to enter an expression similar to ‘ X / 0
+                ‘ to a calculator, you will receive an error. <br />
+                <br /> However, there is an expression that aptly sums up this
+                equation (no pun intended). <br />
+                <br /> As we divide 1 by different numbers, we see the result
+                grow larger and larger. <br />
+                <br /> 1 / 0.1 = 10 <br />
+                <br /> 1 / 0.01 = 100 <br />
+                <br /> 1 / 0.001 = 1000 <br />
+                <br /> From this, we might conclude that X / 0 = infinity, but
+                this gives only half an accurate answer. <br />
+                <br /> What if x is any negative number? <br />
+                <br /> 1 / -0.1 = -10 <br />
+                <br /> 1 / -0.01 = -100 <br />
+                <br /> 1 / -0.001 = -1000 <br />
+                <br /> This shows a set of values leading our equation in the
+                opposite direction; therefore, our expression must cover
+                positive and negative values which can be done by taking the
+                absolute value of each side. <br />
+                <br />| X / 0 | = | ∞ |<br />
+                <br /> This can be read as the absolute value of any number
+                divided by zero is equal to the absolute value of infinity
               </p>
             </Typography>
           </CardContent>
