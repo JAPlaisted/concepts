35c1ac4ca65e7ef6436af5325cd34fc466d56b5b
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-05-31 15:06:17 -0700

    img height

--- src/articles/devportfolios7.js
+++ src/articles/devportfolios7.js
@@ -40,8 +40,8 @@ function DevPortfolios7() {
                   <a href="https://lynnandtonic.com/">
                     <h4>Lynn Fisher</h4>
                     <img
-                      height="20vh"
-                      width="20vh"
+                      height="50vh"
+                      width="50vh"
                       src="https://alvarotrigo.com/blog/assets/imgs/2021-10-16/lynn-fisher-web-developer-portfolio-example.webp"
                       alt="portfolio"
                     ></img>
@@ -51,8 +51,8 @@ function DevPortfolios7() {
                   <a href="http://findmatthew.com/">
                     <h4>Matthew Williams</h4>
                     <img
-                      height="20vh"
-                      width="20vh"
+                      height="50vh"
+                      width="50vh"
                       src="https://alvarotrigo.com/blog/assets/imgs/2021-10-16/matthew-williams-web-developer-portfolio.webp"
                       alt="portfolio"
                     ></img>
@@ -62,8 +62,8 @@ function DevPortfolios7() {
                   <a href="https://www.jonathanplaisted.com/">
                     <h4>Jonathan Plaisted</h4>
                     <img
-                      height="20vh"
-                      width="20vh"
+                      height="50vh"
+                      width="50vh"
                       src="https://user-images.githubusercontent.com/55415399/163905022-44cf83c9-942c-4753-ae7e-79742417c921.PNG"
                       alt="portfolio"
                     ></img>
@@ -73,8 +73,8 @@ function DevPortfolios7() {
                   <a href="https://mattfarley.ca/">
                     <h4>Matt Farley</h4>
                     <img
-                      height="20vh"
-                      width="20vh"
+                      height="50vh"
+                      width="50vh"
                       src="https://cdn-media-1.freecodecamp.org/images/1*FNvXAOU_leQ7E2Bjuc8gqw.png"
                       alt="portfolio"
                     ></img>
@@ -84,8 +84,8 @@ function DevPortfolios7() {
                   <a href="https://www.adhamdannaway.com/">
                     <h4>Adham Dannaway</h4>
                     <img
-                      height="20vh"
-                      width="20vh"
+                      height="50vh"
+                      width="50vh"
                       src="https://cdn.hashnode.com/res/hashnode/image/upload/v1618299482987/s7c24s_NN.png?auto=compress,format&format=webp"
                       alt="portfolio"
                     ></img>
@@ -95,8 +95,8 @@ function DevPortfolios7() {
                   <a href="https://www.seanhalpin.design/">
                     <h4>Sean Halpin</h4>
                     <img
-                      height="20vh"
-                      width="20vh"
+                      height="50vh"
+                      width="50vh"
                       src="https://miro.medium.com/max/1400/0*HmDsRf97mn1Sasi_.png"
                       alt="portfolio"
                     ></img>
@@ -106,8 +106,8 @@ function DevPortfolios7() {
                   <a href="http://taylorkmho.com/">
                     <h4>Taylor Ho</h4>
                     <img
-                      height="20vh"
-                      width="20vh"
+                      height="50vh"
+                      width="50vh"
                       src="https://lh4.googleusercontent.com/cbXcGHgNAq5bzA4-CKi7GUdX3nw6XB7M-NNrPCqHyMJzVos1rguu2Gg2TZ4ZNL37Y0Yj6TlDga9C8vxxtmUGSUPuiySK4pI0vpWsbIKzfnUMfoWw3P4BcFssLgHzUMWLia-AMF_f"
                       alt="portfolio"
                     ></img>
