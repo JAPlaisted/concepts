692dfb0f41a7743e85a676ef23ea9974e440472f
Jonathan Plaisted <55415399+JAPlaisted@users.noreply.github.com>
2022-11-15 14:07:17 -0800

    updated spelling

--- src/articles/topbusbooks.js
+++ src/articles/topbusbooks.js
@@ -39,9 +39,9 @@ function TopBusBooks() {
               <a href="https://www.amazon.com/Zero-to-One-audiobook/dp/B00M284NY2/ref=sr_1_1?crid=3LBGB8A56JLMX&keywords=zero+to+one+peter+thiel&qid=1668549502&sprefix=zero+to+one%2Caps%2C194&sr=8-1">View on Amazon</a>
               <h3>Rich Dad Poor Dad by Robert Kiyosaki</h3>
               <a href="https://www.amazon.com/Rich-Dad-Poor-Anniversary-Middle/dp/B07QPQ8WBL/ref=sr_1_1?crid=2HWFG6TOKFPXW&keywords=rich+dad+poor+dad&qid=1668549546&s=audible&sprefix=rich+dad+poor+dad%2Caudible%2C140&sr=1-1">View on Amazon</a>
-              <h3>The Neatest Little Buide to Stock Market Investing by Jason Kelly</h3>
+              <h3>The Neatest Little Guide to Stock Market Investing by Jason Kelly</h3>
               <a href="https://www.amazon.com/Neatest-Little-Guide-Market-Investing/dp/B088TWCM69/ref=sr_1_1?crid=2CUN9B6EFFNWV&keywords=the+neatest+little+guide+to+stock+market+investing&qid=1668549610&sprefix=the+neatest+%2Caps%2C156&sr=8-1">View on Amazon</a>
-              <h3>The Four by Scott Gallowar</h3>
+              <h3>The Four by Scott Galloway</h3>
               <a href="https://www.amazon.com/The-Four-Scott-Galloway-audiobook/dp/B07565MYD1/ref=sr_1_1?crid=29CX236JDO5ZT&keywords=the+four&qid=1668549657&s=audible&sprefix=the+four%2Caudible%2C169&sr=1-1">View on Amazon</a>
               <h3>Think and Grow Rich by Napolean Hill</h3>
               <a href="https://www.amazon.com/Think-and-Grow-Rich-Napoleon-Hill-audiobook/dp/B000XJNDVQ/ref=sr_1_1?crid=1OGVLSJOZMQLQ&keywords=think+and+grow+rich&qid=1668549699&s=audible&sprefix=think+and+%2Caudible%2C165&sr=1-1">View on Amazon</a>
